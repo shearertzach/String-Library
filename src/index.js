53138a49b6e8f2de2ce247e795cd9a0a5a300f9e
shearertzach <shearertzach@gmail.com>
2022-08-21 19:23:16 -0500

    Kebab Case function completed

--- src/index.js
+++ src/index.js
@@ -14,11 +14,30 @@ function capitalizeWords(sentence) {
   return words.join(' ')
 }
 
-function removeExtraSpace(sentence) {
+function removeExtraSpaces(sentence) {
   const words = sentence.split(' ')
   const newSentence = []
   for (let i = 0; i < words.length; i++) {
     if (words[i] !== '') newSentence.push(words[i])
   }
   return newSentence.join(' ')
+}
+
+function kebabCase(sentence) {
+  const cleanSentence = removeExtraSpaces(sentence)
+  const words = cleanSentence.split(' ')
+  const kebab = []
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i].toLowerCase().split('')
+    const cleanWord = []
+    for (let j = 0; j < word.length; j++) {
+      const letter = word[j]
+      const letterCode = letter.charCodeAt()
+      const isLetter = (letterCode > 96) && (letterCode < 123)
+      const isNumber = (letterCode > 47) && (letterCode < 58)
+      if (isLetter || isNumber) cleanWord.push(letter)
+    }
+    kebab.push(cleanWord.join(''))
+  }
+  return kebab.join('-')
 }
